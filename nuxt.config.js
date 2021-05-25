9e3f8118b9d098292b58feb56014cf4e117a6c67
MrSociety404 <cstfabrice@gmail.com>
2021-05-25 14:19:57 +0200

    meta image

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: `@/assets/image/miniature.png`,
+        content: 'https://i.imgur.com/SbyyjBY.png'
       },
       {
         hid: 'oh:title',
