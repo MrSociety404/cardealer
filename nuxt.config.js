108e6aafec4f36d32c04998fefddefce20c73273
MrSociety404 <cstfabrice@gmail.com>
2021-05-27 12:08:47 +0000

    Fix brand

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Luxury Motorsport',
+    title: 'Sunrise Autos',
     htmlAttrs: {
       lang: 'fr'
     },
