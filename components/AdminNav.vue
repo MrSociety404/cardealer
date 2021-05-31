fc879a2e9059d9792b1c14c29e768b46dd99531f
MrSociety404 <cstfabrice@gmail.com>
2021-05-31 17:10:26 +0200

    admin Nav

--- components/AdminNav.vue
+++ components/AdminNav.vue
@@ -1,42 +1,42 @@
 <template>
   <div class="hidden">
-    <vs-sidebar
-      absolute
-      v-model="active"
-      open
-      >
+    <vs-sidebar absolute v-model="active" open>
       <template #logo>
-        <img src="@/assets/image/LogoBlackOnly.png" alt="logo">
+        <img src="@/assets/image/LogoBlackOnly.png" alt="logo" />
       </template>
-      <vs-sidebar-item id="sales" to="/admin/sales" @click="updateNav(sales)">
+      <vs-sidebar-item id="sales" to="/admin/sales">
         <template #icon>
-          <i class='bx bx-dollar-circle' ></i>
+          <i class="bx bx-dollar-circle"></i>
         </template>
         Ventes
       </vs-sidebar-item>
-      <vs-sidebar-item id="car" to="/admin/car" @click="updateNav(car)">
+      <vs-sidebar-item id="car" to="/admin/car" >
         <template #icon>
-          <i class='bx bx-car'></i>
+          <i class="bx bx-car"></i>
         </template>
         Voitures
       </vs-sidebar-item>
-      <vs-sidebar-item id="dealer" to="/admin/dealer" @click="updateNav(dealer)">
+      <vs-sidebar-item
+        id="dealer"
+        to="/admin/dealer"
+        @click="updateNav(dealer)"
+      >
         <template #icon>
-          <i class='bx bx-briefcase'></i>
+          <i class="bx bx-briefcase"></i>
         </template>
         Vendeurs
       </vs-sidebar-item>
-      <vs-sidebar-item id="job" to="/admin/job" @click="updateNav(job)">
+      <vs-sidebar-item id="job" to="/admin/job">
         <template #icon>
-          <i class='bx bx-user-pin'></i>
+          <i class="bx bx-user-pin"></i>
         </template>
         Job
       </vs-sidebar-item>
       <template #footer>
         <vs-row justify="space-between">
-            <vs-button icon @click="logout" >
-              <i class='bx bx-log-out-circle' ></i>
-            </vs-button>         
+          <vs-button icon @click="logout">
+            <i class="bx bx-log-out-circle"></i>
+          </vs-button>
         </vs-row>
       </template>
     </vs-sidebar>
@@ -44,31 +44,29 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        active: 'sales'
-      }
+export default {
+  data() {
+    return {
+      active: "sales",
+    };
+  },
+  mounted() {
+    this.fetchNav();
+  },
+  methods: {
+    async logout() {
+      await this.$axios.get(
+        "https://cardealer.mrsociety404.com/api/dealers/logout"
+      );
+      this.$router.push("/admin");
     },
-    mounted() {
-      this.fetchNav()
-    },
-    methods: {
-      async logout() {
-        await this.$axios.get('https://cardealer.mrsociety404.com/api/dealers/logout')
-        this.$router.push('/admin')
-      },
-      fetchNav() {
-        if(sessionStorage.getItem("adminNav") !== null) {
-          this.active = sessionStorage.getItem("adminNav")
-        }
-      },
-      updateNav(newActive) {
-        sessionStorage.removeItem("adminNav")
-        sessionStorage.setItem("adminNav", newActive)
+    fetchNav() {
+      if (sessionStorage.getItem("adminNav") !== null) {
+        this.active = sessionStorage.getItem("adminNav");
       }
     }
-  }
+  },
+};
 </script>
 
 <style scopted>
